c84603edc7beda2c16dcf91ab6aec5dfe09dc298
Kaibu <Whocaresaboutgreeny@web.de>
2017-02-15 18:55:51 +0100

    exparam fix

--- config.js
+++ config.js
@@ -5,3 +5,9 @@
 var APIBaseURL = 'https://api2.arctic-network.com/v1/'
 var APIModsURL = 'mods'
 var APIModHashlistURL = 'mod/hashlist/'
+
+
+Array.prototype.extend = function (other_array) {
+  /* you should include a test to check whether other_array really is an array */
+  other_array.forEach(function(v) {this.push(v)}, this);
+}
